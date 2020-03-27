7752c8a0529f83bc4c3784d7316605d8d39f1364
Kavindu8956 <sandeepa8956@gmail.com>
2020-03-27 22:08:48 +0530

    add bootstrap and half css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import InputItem from "./components/InputItem";
+import ItemList from "./components/ItemList";
 
-function App() {
-  return (
-    <h1>Kavindu</h1>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from "uuid";
+
+class App extends Component{
+  render() {
+    return (
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-8 mt-4">
+              <h3 className="text-capitalize text-center">todo input</h3>
+              <InputItem />
+              <ItemList />
+            </div>
+          </div>
+        </div>
+
+
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ItemList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Item from "./Item";
+
+class ItemList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+                <Item />
+            </div>
+
+        );
+    }
+}
+
+export default ItemList;
