9193cbfdacf178271fc366e3d13bfe96c01fc072
Kavindu8956 <sandeepa8956@gmail.com>
2020-03-27 22:38:38 +0530

    item list code

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,11 +4,14 @@ import Item from "./Item";
 class ItemList extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello</h1>
-                <Item />
-            </div>
-
+            <ul className="list-group my-5">
+                <h3 className="text-capitalize text-center">
+                    todo list
+                </h3>
+                <button type="button" className="btn btn-danger btn-block text-capitalize mt-5">
+                    clear list
+                </button>
+            </ul>
         );
     }
 }
