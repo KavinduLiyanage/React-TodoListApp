69c19c42400f8ad8630d6d99f04381177e7134ce
Kavindu8956 <sandeepa8956@gmail.com>
2020-03-28 14:24:01 +0530

    add item complete

--- src/App.js
+++ src/App.js
@@ -3,33 +3,53 @@ import InputItem from "./components/InputItem";
 import ItemList from "./components/ItemList";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import uuid from "uuid";
 
 class App extends Component{
     state = {
         items:[],
-        id:0,
+        id:1,
         item:'',
         editItem:false
     };
+
     handleChange = e => {
         this.setState({
             item: e.target.value
         });
     };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const newItem = {
+            id: this.state.id++,
+            title: this.state.item
+        };
+
+        const updatedItems = [...this.state.items,newItem];
+
+        this.setState({
+            items:updatedItems,
+            item:'',
+            id:this.state.id,
+            editItem:false
+        });
+    };
+
   render() {
     return (
         <div className="container">
           <div className="row">
             <div className="col-10 mx-auto col-md-8 mt-4">
               <h3 className="text-capitalize text-center">todo input</h3>
-              <InputItem item={this.state.item} handleChange={this.handleChange} />
-              <ItemList />
+              <InputItem
+                  item={this.state.item}
+                  handleChange={this.handleChange}
+                  handleSubmit={this.handleSubmit} />
+              <ItemList items={this.state.items} />
             </div>
           </div>
         </div>
-
-
     );
   }
 }
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,13 +3,22 @@ import Item from "./Item";
 
 class ItemList extends Component {
     render() {
+        const {items} = this.props
         return (
             <ul className="list-group my-5">
                 <h3 className="text-capitalize text-center">
                     todo list
                 </h3>
-                <Item />
-                <button type="button" className="btn btn-danger btn-block text-capitalize mt-5">
+
+                {
+                    items.map(item => {
+                        return(
+                            <Item key={item.id} title={item.title} />
+                        )
+                    })
+                }
+
+                <button type="button" className="btn btn-danger btn-block text-capitalize mt-5" >
                     clear list
                 </button>
             </ul>
